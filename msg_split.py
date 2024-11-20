bfe0cdf3a002972d952f96868009c6dbf3e95b99
Анна Мителькова <221882@mail.ru>
2024-11-20 11:32:06 +0100

    test correct splitting

--- msg_split.py
+++ msg_split.py
@@ -1,5 +1,6 @@
 from types import GeneratorType
 from bs4 import BeautifulSoup
+from bs4.element import NavigableString, Tag
 
 
 MAX_LEN = 4096
@@ -17,35 +18,12 @@ def split_message(source: str, max_len=MAX_LEN):# -> GeneratorType[str]:
 
     soup = BeautifulSoup(source, 'html.parser') # creates a tag-tree
 
-    # print(soup.prettify())
-
-    # recursively goes deep in inner tags    
-    for el in soup.recursiveChildGenerator():
-        tag = el.name
-        t = el.text
-        if isinstance(el, str):  # Если текст
-            curr_text = el.strip()
-        else:  # Если тег
-            curr_text = el.text.strip()
-        # curr_text = str(el)
-        curr_text_len = len(curr_text)
-
-
-        # finish fragment or raise error
-        if curr_fragment_len + curr_text_len > max_len:
-            # can not continue the fragment
-            if not curr_fragment:
-                raise ValueError(f"Message is too long to be split into fragments of length {max_len}")
-            
-            # finish fragment
-            yield ''.join(curr_fragment)
-
-            # start new fragment
-            curr_fragment = []
-            curr_fragment_len  = 0
-        
-        curr_fragment.append(curr_text)
-        curr_fragment_len += curr_text_len
+    for child in soup.descendants:
+        if isinstance(child, NavigableString):
+            words = child.split()
+            print('text', words)
+        elif isinstance(child, Tag):
+            print('tag', [str(child)])
 
 
 
@@ -53,8 +31,18 @@ def test():
     source = "<p>Hello, <b>world</b>!</p>"
     max_len = 10
     res = split_message(source, max_len)
-    for _ in res:
-        print(_)
+    # for _ in res:
+    #     print(_)
+
+
+# Тестирование функции
+def test():
+    # source = "<p>Hello, <b>world</b>! This is a <i>test</i> message with <u>HTML</u> tags.</p>"
+    source = "<p>Hello, <b>world</b>! This is a <i>long</i> message.</p>"
+    max_len = 20
+    res = split_message(source, max_len)
+    for chunk in res:
+        print(chunk)
 
 if __name__ == "__main__":
     test()
