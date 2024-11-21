228a7696085dd4d4eb86dca9de2c09305857f481
Анна Мителькова <221882@mail.ru>
2024-11-21 21:04:21 +0100

    need meditation

--- msg_split.py
+++ msg_split.py
@@ -15,18 +15,43 @@ def split_message(source: str, max_len=MAX_LEN):# -> GeneratorType[str]:
     length (`max_len`)
     """
     chunk = ''
+    full_chunk_len = 0
     tags = [] # list so that i can go through to form open and closing tags
     soup = BeautifulSoup(source, 'html.parser')
 
+    def add_opening_tags(tags):
+        """Возвращает строку открывающих тегов."""
+        return "".join(f"<{tag}>" for tag in tags)
+
+    def add_closing_tags(tags):
+        """Возвращает строку закрывающих тегов для всех открытых тегов."""
+        return "".join(f"</{tag}>" for tag in reversed(tags))
+
     def process_tag(tag):
-        nonlocal chunk
+        nonlocal chunk, full_chunk_len
         tag_name = tag.name
         tag_str = f'<{tag.name}>'
-        tags_brackets_len = len(tag_name) * 2 + 5 # 2 tag names + <> + </>
+        tags.append(tag_name)
+
+        tags_open = add_opening_tags(tags)
+        tags_close = add_closing_tags(tags)
+        all_tags_len = len(tags_open) + len(tags_close)
         tags.append(tag.name)
-        chunk_len = len(chunk)
-        if tags_brackets_len + chunk_len <= max_len:
+        len_chunk = len(chunk)
+        if all_tags_len + len_chunk <= max_len: # if we can potentially close curr tag
             chunk += tag_str
+            full_chunk_len = all_tags_len
+        else:
+            chunk += add_closing_tags
+        return chunk
+    
+    def process_text(text):
+        nonlocal chunk
+        len_text = len(text)
+        if len_text + full_chunk_len <= max_len:
+            chunk += str(text)
+        else:
+            pass
         return chunk
 
 
@@ -34,11 +59,13 @@ def split_message(source: str, max_len=MAX_LEN):# -> GeneratorType[str]:
     for child in soup.recursiveChildGenerator():
         if isinstance(child, Tag):  # Обрабатываем тег
             print(process_tag(child))
+        elif isinstance(child, NavigableString):
+            print(process_text(child))
 
 
 # Тестирование
 html = "<p>Hello, <b>world</b>!</p>"
-max_len = 10
+max_len = 50
 
 result = split_message(html, max_len)
 
